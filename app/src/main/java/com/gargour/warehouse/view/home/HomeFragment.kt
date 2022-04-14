786b0f313b8b4badb2804c9ce2a3e01f98b70055
Beshoy Adel <beshoyadelisk@gmail.com>
2022-04-14 15:34:24 +0200

    modify login and register ui.

--- app/src/main/java/com/gargour/warehouse/view/home/HomeFragment.kt
+++ app/src/main/java/com/gargour/warehouse/view/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gargour.warehouse.databinding.FragmentHomeBinding
 import com.gargour.warehouse.util.ViewExt.showToast
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +24,14 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.scrollLayout.apply {
+            receiveLinear.setOnClickListener { }
+            issueLinear.setOnClickListener { }
+            transferLinear.setOnClickListener { }
+            returnsLinear.setOnClickListener { }
+            settingsLinear.setOnClickListener { }
+            logOutLinear.setOnClickListener { }
+        }
     }
 
     private fun showError(message: String) {
---
+++ app/src/main/java/com/gargour/warehouse/view/home/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.gargour.warehouse.view.home
+
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavDirections
+import com.gargour.warehouse.util.DispatcherProvider
+import com.gargour.warehouse.util.SingleEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val dispatcherProvider: DispatcherProvider) :
+    ViewModel() {
+
+    private val _navigationEvent = MutableLiveData<SingleEvent<NavDirections>>()
+    val navigationEvent: LiveData<SingleEvent<NavDirections>> get() = _navigationEvent
+
+    fun logout(preferences: SharedPreferences) {
+        viewModelScope.launch(dispatcherProvider.io) {
+            val editor = preferences.edit()
+            editor.clear()
+            editor.apply()
+            //todo(1) navigate back to login
+//            _navigationEvent.postValue(SingleEvent())
+        }
+    }
+}
